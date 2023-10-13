4a1c046f56db7852430eaab43e20a1424953a3c8
jpsantostefano <jpsantostefano@gmail.com>
2023-10-13 21:46:29 +0000

    template index created and linked with url

--- whereToEat/views.py
+++ whereToEat/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from django.template import Template, Context
 # Create your views here.
+
+def index(request):
+    #Open the document
+    index = open("templates/index.html")
+    #Load the document
+    template = Template(index.read())
+    #Close the document
+    index.close()
+    #Create a context
+    context = Context()
+    #Rendering the document
+    document = template.render(context)
+    return HttpResponse(document)
